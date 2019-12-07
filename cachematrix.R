5df59a9a744f3fca588ca01b3264d040e3fdeaa3
Martin de Jong <mdejong@pro4all.nl>
2019-12-07 17:49:25 +0100

    Comments updated

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,12 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 'makeCacheMatrix' is a function that creates a matrix that can store both is own data and its inverse.
+## 'cacheSolve' is a function that checkes a CacheMatrix, return its inverse. It calculates the inverse if needed and stores it in the CacheMatrix.
 
-## Write a short comment describing this function
+## This function is used to create a matrix that can store its own inverse.
+## It creates a lexical scope that hold two variables: 
+## The x which holds  matrix and the i whih can hold its inverse.
+## This function returns a list with four functions:
+## The 'get' and 'set' functions to get and set the matrix
+## The 'getinverse' and 'setinverse' functions to get and set the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
@@ -18,7 +23,16 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function gets the inverse of the cacheMatrix that is passed as its parameter.
+## It calls the getinverse() function of cacheMatrix.
+##
+## If the result of the this function is not NULL, the inverse was cached in the cacheMatrix.
+## This result is then returned using the return() fuction, which end the execution of the cacheSolve function.
+## 
+## Otherwise, the get function is used on the cacheMatrix to get the matrix.
+## Then the solve function is called to get the inverse of this matrix.
+## This inverse is stored in the cacheMatrix using the setinverse method.
+## the inverse is then returned.
 
 cacheSolve <- function(x, ...) {
   i <- x$getinverse()
